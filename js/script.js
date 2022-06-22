edbe3712f52472d588066629950a5ff9f686a07f
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2022-06-22 16:16:23 +0200

    added button event

--- js/script.js
+++ js/script.js
@@ -12,9 +12,33 @@ va applicato uno sconto del 40% per gli over 65.
 Il recap dei dati e l'output del prezzo finale va stampato in pagina
 (formattato con massimo due decimali, per indicare centesimi sul prezzo).
 
+/*
+1- 
+2-
+3- 
+
 */
+console.log("JS OK")
+
+const inputName = document.getElementById("name");
+const inputNumber = document.getElementById("km");
+const buttonGen = document.getElementById("btn-genera");
+const buttonAnn = document.getElementById("btn-annulla");
+const age = document.getElementById("age");
+const totalPrice = inputNumber.value * 0.21;
 
 
+
+
+buttonGen.addEventListener('click', function () {
+
+    console.log("mi hai cliccato!");
+    console.log(inputName.value);
+    console.log(inputNumber.value);
+    console.log(age.value);
+    console.log(totalPrice);
+
+});
 
 
 
